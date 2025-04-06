1e908971832681bcf58a48d78b09a74f9ca283ce
本川　晴彦 <harx6009@icloud.com>
2025-04-06 20:39:09 +0900

    TodoおよびUserモデルを追加し、メインアプリケーションの基本構造を実装

---
+++ lib/domain/models/todo.dart
@@ -0,0 +1,11 @@
+class Todo {
+  Todo({
+    required this.id,
+    required this.title,
+    required this.isCompleted,
+  });
+  final String id;
+  final String title;
+
+  final bool isCompleted;
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MainApp()));
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomeScreen());
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) =>
+      const Scaffold(body: Center(child: Text('Hello World!')));
+}
